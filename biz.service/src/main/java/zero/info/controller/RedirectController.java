6de3e380ecf7cf09342346775e83cc12a7d687a3
hulkC <1036846223@qq.com>
2024-01-10 23:41:09 +0800

    update code

--- biz.service/src/main/java/zero/info/controller/RedirectController.java
+++ biz.service/src/main/java/zero/info/controller/RedirectController.java
@@ -1,21 +1,25 @@
 package zero.info.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
+@Slf4j
 public class RedirectController {
 
     private static final String baseUrl = "http://103.144.28.34:10000/";
 
     @RequestMapping("/test")
     public RedirectView redirectWithUsingRedirectTestView() {
+        log.info("test,redirectSuc");
         return new RedirectView(baseUrl + "dataCraw1.html");
     }
 
     @RequestMapping("/")
     public RedirectView redirectWithUsingRedirectView() {
+        log.info("stable,redirectSuc");
         return new RedirectView(baseUrl + "dataCraw.html");
     }
 }
